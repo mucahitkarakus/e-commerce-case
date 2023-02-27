dec799b9e3720feb39733348d81c2d873b7c8c98
mucahitkarakus <mocococo.80@gmail.com>
2023-02-28 02:39:15 +0300

    card ui and home

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 const Card = () => {
+    const navigate = useNavigate();
   return (
-<div className='w-[75%] m-5 p-5 flex mx-auto cursor-pointer'
-    >
-        <p><span className='text-xl font-bold'> Lorem, ipsum dolor.</span>  <br /> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate, enim!</p>
+
+    <div className='w-[75%] m-5 p-5 flex mx-auto cursor-pointer'>
+        <div onClick={() => navigate("/post")}>
+            <h2 className='font-bold mb-4'>Lorem ipsum dolor sit amet.</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, doloribus deleniti excepturi ut consectetur laboriosam odio perspiciatis molestiae error exercitationem.</p>
+        </div>
+
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '../components/Card/Card'
 
 const Home = () => {
+
   return (
     <div>
          <div className='bg-slate-100 mt-10 w-full grid grid-cols-3 ' >
