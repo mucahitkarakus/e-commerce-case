bab02b14b5244c301a030ac1826908b0426f9bf6
mucahitkarakus <mocococo.80@gmail.com>
2023-02-28 02:29:13 +0300

    navbar ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { AiTwotonePlayCircle } from 'react-icons/ai'
+import { IoMdNotifications } from 'react-icons/io'
+import { RxHamburgerMenu } from 'react-icons/rx'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <div className='bg-slate-100 h-12 m-8 '>
+        <div className='flex items-center justify-between m-6 '>
+            <div className='flex'> 
+            <AiTwotonePlayCircle className='w-8 h-8 mr-5' />
+            <h2>Arbit Blog</h2>
+            </div>
+        <div className='flex'> 
+            <button className='mr-5'>Posts</button>
+            <IoMdNotifications className='w-6 h-6 mr-5' />
+            <RxHamburgerMenu className='w-6 h-6 mr-5' />
+            <img src="https://imgs.search.brave.com/lRriKfKIzH4MtRngWNqkXn2bfnax3VMC72vrtew3N2E/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5X/LUpwbnNzc0dieFZa/U01LUy0tZk5RSGFF/OCZwaWQ9QXBp" alt="img" className='w-8 h-8 rounded-full' />
+        </div>
+        </div>
+    </div>
   )
 }
 
